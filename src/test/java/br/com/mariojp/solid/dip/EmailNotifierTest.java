50090b6e35d0ac903107bc03ff6b095f0018328f
micaeltanner-byte <micael.tanner@ucsal.edu.br>
2025-09-02 22:04:15 -0300

    questao 6

--- src/test/java/br/com/mariojp/solid/dip/EmailNotifierTest.java
+++ src/test/java/br/com/mariojp/solid/dip/EmailNotifierTest.java
@@ -1,14 +1,18 @@
 package br.com.mariojp.solid.dip;
 
+import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
 import org.junit.jupiter.api.Test;
-import static org.junit.jupiter.api.Assertions.*;
 
 public class EmailNotifierTest {
+
     @Test
     void dry_run_should_not_touch_smtp() {
         System.setProperty("DRY_RUN", "true");
         System.clearProperty("SMTP_AVAILABLE");
-        var notifier = new EmailNotifier();
+
+        MailSender mailSender = new NoopMailSender();
+        var notifier = new EmailNotifier(mailSender);
+
         assertDoesNotThrow(() -> notifier.welcome(new User("Ana", "ana@example.com")),
                 "Após refatoração (DIP), DRY_RUN deve evitar SMTP real");
     }
